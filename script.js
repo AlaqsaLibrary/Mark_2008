7f8daee7e70cf7999f72caae1fd9ec613422ad50
AlaqsaLibrary <Alaqsalibrary2016@gmail.com>
2025-08-20 13:43:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function calculate() {
   let total = arabic + english + islamic + history;
   let maxTotal = 100 + 100 + 60 + 40; // مجموع العلامات الكاملة
   let percentage = ((total / maxTotal) * 100).toFixed(2);
+  let percentage3 = ((total / 10) ).toFixed(2);
 
+  document.getElementById("result3").innerHTML = " النسبةالمئوية من 30: " + percentage3 + " %";
   document.getElementById("result").innerHTML = "النسبة المئوية: " + percentage + " %";
 }
